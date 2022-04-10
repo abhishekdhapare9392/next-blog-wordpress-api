faf35bbac85593f91d6a8dc3877a800f6daa71bf
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2022-04-10 11:23:15 +0530

    initial commit

---
+++ pages/[slug].js
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const BlogDetails = ({ post }) => {
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-12 col-sm-12">
+              <h1>
+                <Link href="/">
+                  <a className="btn btn-primary"> Back</a>
+                </Link>
+                &nbsp;&nbsp;
+                {post.title.rendered}
+              </h1>
+              <Image
+                width={500}
+                height={250}
+                layout="responsive"
+                src={post.image}
+                alt="Card image cap"
+              />
+              <div className="py-3">
+                <h6>
+                  Published On:&nbsp;
+                  <span className="text-muted">
+                    {post.date.split("T")[0].split("-").reverse().join("-")}
+                  </span>{" "}
+                  &nbsp; &nbsp; Author: &nbsp;
+                  <span className="text-muted">{post.authorName}</span>
+                </h6>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: post.content.rendered,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+
+  //   console.log(params.slug);
+  const res = await fetch(
+    `http://wp.socialubiquity.local/wp-json/wp/v2/posts?slug=${params.slug}`,
+  );
+  const data = await res.json();
+  for (const key in data) {
+    const imageMedia = await fetch(
+      `http://wp.socialubiquity.local/wp-json/wp/v2/media/${data[key].featured_media}`,
+    );
+    const imageMediaData = await imageMedia.json();
+    data[key].image = imageMediaData.source_url;
+
+    const authorDetails = await fetch(
+      `http://wp.socialubiquity.local/wp-json/wp/v2/users/${data[key].author}`,
+    );
+    const authorData = await authorDetails.json();
+    data[key].authorName = authorData.name;
+  }
+
+  console.log(data);
+
+  return {
+    props: {
+      post: data[0],
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch("http://wp.socialubiquity.local/wp-json/wp/v2/posts");
+  const data = await res.json();
+  //   console.log(data);
+
+  const slugs = data.map((post) => ({ params: { slug: post.slug } }));
+
+  return {
+    paths: slugs,
+    fallback: true,
+  };
+};
+
+export default BlogDetails;
